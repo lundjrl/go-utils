3b107102c5fa910e1a91f3e7d13c2c2e98c19c7b
jam <jrlbidamin@gmail.com>
2025-11-15 17:09:46 -0500

    Added loop for reading network interfaces

--- net/main.go
+++ net/main.go
@@ -9,7 +9,6 @@ import (
 
 func main() {
 	interfaces, err := net.Interfaces()
-
 	if err != nil {
 		log.Error("could not get network interfaces", err)
 		os.Exit(0)
@@ -19,5 +18,16 @@ func main() {
 
 	for _, iface := range interfaces {
 		log.Info("interface name: ", iface.Name)
+		log.Info("interface addres: ", iface.HardwareAddr)
+
+		addresses, err := iface.Addrs()
+		if err != nil {
+			log.Error("cannot read address", err)
+		}
+
+		for _, address := range addresses {
+			log.Info("network address:", address.String())
+		}
+		log.Info("\n")
 	}
 }
